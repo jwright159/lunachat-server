c8d3fb64540ecf16e3b08bd520a9621fcdbace02
Jade Wright <iamjwright159@gmail.com>
2022-09-14 19:30:34 -0400

    Make a working server

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+import asyncio
+from websockets.server import serve, WebSocketServerProtocol
 
-app = Flask(__name__)
+async def handler(websocket: WebSocketServerProtocol):
+	print(f"Connected to {websocket.host}")
+	async for message in websocket:
+		await websocket.send(f"Got {message}")
+	print(f"Disconected from {websocket.host}")
 
-@app.get('/')
-def index():
-	return {
-		'result': 'bepis'
-	}
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+start_server = serve(handler, 'localhost', 8000)
+
+loop.run_until_complete(start_server)
+loop.run_forever()
