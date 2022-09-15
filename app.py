fc76b8642547bcbc39fa234f00d2028826335724
Jade Wright <iamjwright159@gmail.com>
2022-09-14 23:05:01 -0400

    Add like an actual api

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import asyncio
+import json
 from websockets.server import serve, WebSocketServerProtocol
 
 async def handler(websocket: WebSocketServerProtocol):
 	print(f"Connected to {websocket.host}")
+	username = 'Non-logged-in User'
 	async for message in websocket:
-		await websocket.send(f"Got {message}")
+		data = json.loads(message)
+		match data['type']:
+			case 'login':
+				username = data['username']
+				await websocket.send(json.dumps({
+					'type': 'login',
+					'username': username,
+				}))
+			case 'post':
+				await websocket.send(json.dumps({
+					'type': 'post',
+					'username': username,
+					'text': data['text'],
+				}))
+			case _:
+				await websocket.send(json.dumps({
+					'type': 'error',
+					'error': 'Invalid type of message',
+				}))
 	print(f"Disconected from {websocket.host}")
 
 loop = asyncio.new_event_loop()
