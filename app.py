ca7ed27eba34146e18902d53606f600aa4107864
Jade Wright <iamjwright159@gmail.com>
2022-10-05 10:16:27 -0400

    Refactor everything to be more object oriented, add registration

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from abc import ABC
 import abc
 import asyncio
+from dataclasses import dataclass
 import json
+from types import TracebackType
+from typing import Any, Callable, ParamSpec
 import uuid
 from websockets.server import serve, WebSocketServerProtocol
+import bcrypt
+
+P = ParamSpec('P')
 
 Data = str | int | float | list['Data'] | dict[str, 'Data']
 
+@dataclass
 class User:
-	def __init__(self, *, username: str, color: str, websocket: WebSocketServerProtocol):
-		self._generate_id()
-		while self.id in users:
-			self._generate_id()
-		self.username = username
-		self.color = color
-		self.websocket = websocket
-	
-	def _generate_id(self):
-		self.id = uuid.uuid4().hex[:16]
+	id: str
+	username: str
+	password: bytes
+	color: str
+	connections: list['WebsocketConnection']
 	
 	def json(self) -> dict[str, Data]:
 		return {
@@ -53,7 +55,9 @@ class UserPacket(Packet):
 		self.user = user
 
 	async def _send(self):
-		await self.user.websocket.send(json.dumps(self.data))
+		loop = asyncio.get_running_loop()
+		for conn in self.user.connections:
+			loop.create_task(conn.websocket.send(json.dumps(self.data)))
 	
 	def __str__(self):
 		return f"{self.data['type']} to {self.user.username}"
@@ -67,84 +71,164 @@ class ErrorPacket(Packet):
 		await self.websocket.send(json.dumps(self.data))
 	
 	def __str__(self):
-		return f"{self.data['type']} to {self.websocket.host}"
-
-
-users: dict[str, User] = {}
-
-def sender_packets(sender: User, type: str, data: dict[str, Data]) -> list[Packet]:
-	return [UserPacket(sender, type, data)]
-
-def all_packets(type: str, data: dict[str, Data]) -> list[Packet]:
-	return [UserPacket(user, type, data) for user in users.values()]
-
-def all_but_sender_packets(sender: User, type: str, data: dict[str, Data]) -> list[Packet]:
-	return [UserPacket(user, type, data) for user in users.values() if user is not sender]
-
-def error_packets(sender: WebSocketServerProtocol, error: str) -> list[Packet]:
-	return [ErrorPacket(sender, error)]
-
+		return f"{self.data['type']} {self.data['error']} to {self.websocket.host}"
+		
 async def send(packets: list[Packet]):
-	await asyncio.wait([asyncio.create_task(packet.send()) for packet in packets])
+	loop = asyncio.get_running_loop()
+	return [loop.create_task(packet.send()) for packet in packets]
 
-async def handler(websocket: WebSocketServerProtocol):
-	print(f"Connected to {websocket.host}")
+class WebsocketConnection:
+	def __init__(self, app: 'App', websocket: WebSocketServerProtocol):
+		self.app = app
+		self.websocket = websocket
+		self.sender: User | None = None
 
-	sender: User | None = None
+	def __enter__(self):
+		print(f"Connected to {self.websocket.host}")
+		return self
 
-	try:
-		async for message in websocket:
-			try:
-				data = json.loads(message)
-
-				match data['type']:
-					case 'login':
-						if sender is None:
-							sender = User(
-								username=data['username'],
-								color=data['color'],
-								websocket=websocket,
-							)
-							users[sender.id] = sender
-							await send(
-								sender_packets(sender, 'loginSelf', {
-									'me': sender.json(),
-									'users': [user.json() for user in users.values() if user is not sender],
-								}) +
-								all_but_sender_packets(sender, 'login', {
-									'user': sender.json()
-								})
-							)
-						else:
-							await send(error_packets(websocket, "Already logged in"))
-					
-					case 'post':
-						if sender is None:
-							await send(error_packets(websocket, "Not logged in"))
-						else:
-							await send(all_packets('post', {
-								'user': sender.id,
-								'text': data['text'],
-							}))
-					
-					case _:
-						await send(error_packets(websocket, "Invalid type of message"))
-				
-			except Exception as e:
-				await send(error_packets(websocket, f"Error: {e}"))
-	
-	finally:
-		if (sender):
-			users.pop(sender.id)
-			print(f"Disconnected from {sender}")
+	def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None):
+		if self.sender:
+			self.sender.connections.remove(self)
+			host = str(self.sender)
 		else:
-			print(f"Disconected from {websocket.host}")
+			host = str(self.websocket.host)
+		print(f"Disconected from {host} with {exc_value}")
+	
+	async def handle_message(self, message: str | bytes):
+		try:
+			data = json.loads(message)
+			if data['type'] in self.app.handlers:
+				handler = self.app.handlers[data['type']]
+				await send(handler(self, data))
+			else:
+				await send(self.error_packets("Invalid type of message"))
+		except Exception as e:
+			await send(self.error_packets(f"Internal error: {e}"))
+	
+	def sender_packets(self, type: str, data: dict[str, Data]) -> list[Packet]:
+		assert self.sender
+		return self.app.sender_packets(self.sender, type, data)
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+	def all_packets(self, type: str, data: dict[str, Data]) -> list[Packet]:
+		return self.app.all_packets(type, data)
 
-start_server = serve(handler, 'localhost', 8000)
+	def all_but_sender_packets(self, type: str, data: dict[str, Data]) -> list[Packet]:
+		assert self.sender
+		return self.app.all_but_sender_packets(self.sender, type, data)
 
-print("Server started")
-loop.run_until_complete(start_server)
-loop.run_forever()
+	def error_packets(self, error: str) -> list[Packet]:
+		return self.app.error_packets(self.websocket, error)
+			
+Handler = Callable[[WebsocketConnection, Any], list[Packet]]
+
+class App:
+	handlers: dict[str, Handler] = {}
+
+	@classmethod
+	def handler(cls, handler: Handler):
+		cls.handlers[handler.__name__] = handler
+		return handler
+	
+	def __init__(self):
+		self.users: dict[str, User] = {}
+		self.users_by_username: dict[str, User] = {}
+
+	async def websocket_handler(self, websocket: WebSocketServerProtocol):
+		with WebsocketConnection(self, websocket) as conn:
+			async for message in websocket:
+				await conn.handle_message(message) # should i have a try here?
+		
+	def sender_packets(self, sender: User, type: str, data: dict[str, Data]) -> list[Packet]:
+		return [UserPacket(sender, type, data)]
+
+	def all_packets(self, type: str, data: dict[str, Data]) -> list[Packet]:
+		return [UserPacket(user, type, data) for user in self.users.values()]
+
+	def all_but_sender_packets(self, sender: User, type: str, data: dict[str, Data]) -> list[Packet]:
+		return [UserPacket(user, type, data) for user in self.users.values() if user is not sender]
+
+	def error_packets(self, sender: WebSocketServerProtocol, error: str) -> list[Packet]:
+		return [ErrorPacket(sender, error)]
+
+def generate_id():
+	return uuid.uuid4().hex[:16]
+
+@App.handler
+def register(conn: WebsocketConnection, data: Any) -> list[Packet]:
+	if conn.sender is not None:
+		return conn.error_packets("Already logged in")
+	
+	if data['username'] in conn.app.users_by_username:
+		return conn.error_packets("Username taken")
+	
+	user_id = generate_id()
+	while user_id in conn.app.users:
+		user_id = generate_id()
+	conn.sender = User(
+		id=user_id,
+		username=data['username'],
+		password=bcrypt.hashpw(bytes(data['password'], encoding='utf-8'), bcrypt.gensalt()),
+		color='#000000',
+		connections=[conn],
+	)
+	conn.app.users[conn.sender.id] = conn.sender
+	conn.app.users_by_username[conn.sender.username] = conn.sender
+	return (
+		conn.sender_packets('loginSelf', {
+			'me': conn.sender.json(),
+			'users': [user.json() for user in conn.app.users.values() if user is not conn.sender],
+		}) +
+		conn.all_but_sender_packets('login', {
+			'user': conn.sender.json()
+		})
+	)
+
+@App.handler
+def login(conn: WebsocketConnection, data: Any) -> list[Packet]:
+	if conn.sender is not None:
+		return conn.error_packets("Already logged in")
+	
+	if data['username'] not in conn.app.users_by_username:
+		return conn.error_packets("No user with that username")
+
+	attempted_user: User = conn.app.users_by_username[data['username']]
+	if not bcrypt.checkpw(bytes(data['password'], encoding='utf-8'), attempted_user.password):
+		return conn.error_packets("Wrong password")
+
+	conn.sender = attempted_user
+	conn.sender.connections.append(conn)
+	return (
+		conn.sender_packets('loginSelf', {
+			'me': conn.sender.json(),
+			'users': [user.json() for user in conn.app.users.values() if user is not conn.sender],
+		}) +
+		conn.all_but_sender_packets('login', {
+			'user': conn.sender.json()
+		})
+	)
+
+@App.handler
+def post(conn: WebsocketConnection, data: Any) -> list[Packet]:
+	if conn.sender is None:
+		return conn.error_packets("Not logged in")
+	
+	return conn.all_packets('post', {
+		'user': conn.sender.id,
+		'text': data['text'],
+	})
+
+
+def main():
+	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(loop)
+
+	app = App()
+	start_server = serve(app.websocket_handler, 'localhost', 8000)
+
+	print("Server started")
+	loop.run_until_complete(start_server)
+	loop.run_forever()
+
+if __name__ == '__main__':
+	main()
